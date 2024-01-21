dd21bfa288f04b58f12e402f5df1f0fca05b5637
Asadullokh <uzbdevit@gmail.com>
2024-01-21 09:25:06 +0500

    added some more icons

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Photo.css";
 
 const Photo = ({ photo }) => {
-  console.log(photo);
+  const [open, setOpen] = useState(false);
   return (
     <div className="photo ">
       <img className="image" src={photo.image.url} alt="" />
       <div className="flex-elements">
         <p className="photo-title">{photo?.title}</p>
 
-        <span className="photo-delete fa-solid fa-trash"></span>
+        {open && (
+          <>
+            <span className="photo-delete fa-solid fa-trash"></span>
+            <span className="photo-update fa-solid fa-pen"></span>
+            <a
+              download={photo?.image.url}
+              href=""
+              className="photo-download fa-solid fa-download"
+            ></a>
+          </>
+        )}
+
+        <span
+          onClick={() => setOpen(!open)}
+          className={
+            open
+              ? "photo-close fa-solid fa-xmark"
+              : "photo-open fa-solid fa-gear"
+          }
+        ></span>
       </div>
     </div>
   );
